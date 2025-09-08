8a46acef959f955faff5c645e05ca689ae2a71c6
Sai Rahul Paluvai <sprahul333@gmail.com>
2025-09-09 00:16:31 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -638,7 +638,7 @@ networks:
     <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-purple-50">
       {/* Animated Background Pattern */}
       <div className="fixed inset-0 opacity-5">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%239C92AC\" fill-opacity=\"0.1\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-blue-500/5 to-transparent opacity-20"></div>
       </div>
 
       {/* Hero Section */}
